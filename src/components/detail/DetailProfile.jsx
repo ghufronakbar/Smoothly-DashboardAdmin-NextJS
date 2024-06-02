4832acc7862ac89a41301490452789102a875c9b
ghufron akbar <alan.040219.gam@gmail.com>
2024-06-03 01:06:48 +0700

    Profile

--- src/components/detail/DetailProfile.jsx
+++ src/components/detail/DetailProfile.jsx
@@ -19,93 +19,85 @@ import { Loading } from "../Loading";
 export function DetailProfile() {
   const toast = useToast();
   const [loading, setLoading] = useState(true);
-  const [organizationName, setOrganizationName] = useState("");
+  const [nama, setNama] = useState("");
   const [logo, setLogo] = useState(null);
-  const [phone, setPhone] = useState("");
-  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [latitude, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
   const [oldPassword, setOldPassword] = useState("");
   const [newPassword, setNewPassword] = useState("");
   const [confirmationPassword, setConfirmationPassword] = useState("");
   const [profile, setProfile] = useState(null);
-  const [error, setError]=useState(false)
+  const [error, setError] = useState(false);
 
   const { data: dataProfile, refetch: refetchDataProfile } = useQuery({
     queryKey: ["profile"],
     queryFn: async () => {
       const { data } = await axiosInstanceAuthorization.get(`/profile`);
-      const profileData = data[0];
+      const profileData = data.rows[0];
       setProfile(profileData);
-      setOrganizationName(profileData.organization_name);
-      setPhone(profileData.phone);
-      setEmail(profileData.email);
+      setNama(profileData.nama);
+      setUsername(profileData.username);
+      setLatitude(profileData.toko_latitude);
+      setLongitude(profileData.toko_longitude);
       setLoading(false);
       return data;
     },
   });
 
-  
   const handleUpdate = async () => {
-    const formData = new FormData();
-    formData.append("organization_name", organizationName);
-    formData.append("phone", phone);
-    formData.append("email", email);
-    if (logo) {
-      formData.append("logo", logo);
-    }
-
     try {
-      const response = await axiosInstanceAuthorization.put(`/profile/edit`, formData, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
+      const response = await axiosInstanceAuthorization.put(`/profile`, {
+        nama,
+        username,
       });
 
       toast({
-        title: "Successfully updated profile",
+        title: response.data.message,
         status: "success",
+        position: "bottom-right",
+        isClosable: true,
       });
       refetchDataProfile();
     } catch (error) {
       console.error("Error updating profile:", error);
       toast({
-        title: "Failed to update profile",
-        description: error.response?.data?.message || "An unexpected error occurred",
+        title: error.response.data.message,
         status: "error",
+        position: "bottom-right",
+        isClosable: true,
       });
     }
   };
 
   const handlePassword = async () => {
     try {
-      if (newPassword === confirmationPassword) {
-        await axiosInstanceAuthorization.put(`/profile/edit/password`, {
-          old_password: oldPassword,
-          new_password: newPassword,
-        });
-        toast({
-          title: "Successfully changed password",
-          status: "success",
-        });
-        setOldPassword("");
-        setNewPassword("");
-        setConfirmationPassword("");
-      } else {
-        toast({
-          title: "Confirmation password doesn't match",
-          status: "error",
-        });
-      }
+      const response = await axiosInstanceAuthorization.put(`/profile/password`, {
+        old_password: oldPassword,
+        new_password: newPassword,
+        confirmation_password: confirmationPassword,
+      });
+      toast({
+        title: response.data.message,
+        status: "success",
+        position: "bottom-right",
+        isClosable: true,
+      });
+      setOldPassword("");
+      setNewPassword("");
+      setConfirmationPassword("");      
     } catch (error) {
       console.error("Error changing password:", error);
       toast({
-        title: "Failed to change password",
-        description: error.response?.data?.message || "An unexpected error occurred",
+        title: error.response.data.message,
         status: "error",
+        position: "bottom-right",
+        isClosable: true,
       });
     }
   };
 
-  if (loading) return <Loading/>;
+  if (loading) return <Loading />;
   if (error) return <div>Error fetching data</div>;
 
   return (
@@ -121,55 +113,15 @@ export function DetailProfile() {
             flex={2}
           >
             <form>
-              <Center>
-                {logo ? (
-                  <Image
-                    src={URL.createObjectURL(logo)}
-                    alt="Selected Logo"
-                    layout="fill"
-                    boxSize="150px"
-                    borderRadius="50%"
-                    objectFit="cover"
-                  />
-                ) : (
-                  profile.logo && (
-                    <Image
-                      src={profile.logo}
-                      alt="Organization Logo"
-                      layout="fill"
-                      boxSize="150px"
-                      borderRadius="50%"
-                      objectFit="cover"
-                    />
-                  )
-                )}
-              </Center>
               <FormControl my={6}>
-                <FormLabel>Logo</FormLabel>
-                <Input
-                  type="file"
-                  onChange={(e) => setLogo(e.target.files[0])}
-                />
+                <FormLabel>Nama</FormLabel>
+                <Input value={nama} onChange={(e) => setNama(e.target.value)} />
               </FormControl>
               <FormControl my={6}>
-                <FormLabel>Organization Name</FormLabel>
+                <FormLabel>Username</FormLabel>
                 <Input
-                  value={organizationName}
-                  onChange={(e) => setOrganizationName(e.target.value)}
-                />
-              </FormControl>
-              <FormControl my={6}>
-                <FormLabel>Phone</FormLabel>
-                <Input
-                  value={phone}
-                  onChange={(e) => setPhone(e.target.value)}
-                />
-              </FormControl>
-              <FormControl my={6}>
-                <FormLabel>Email</FormLabel>
-                <Input
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
                 />
               </FormControl>
               <VStack>
