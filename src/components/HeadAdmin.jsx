ac79ed2b3236af587772c77a8cadd691fca8a300
ghufron akbar <alan.040219.gam@gmail.com>
2024-06-03 01:08:22 +0700

    Head

--- src/components/HeadAdmin.jsx
+++ src/components/HeadAdmin.jsx
@@ -1,23 +1,11 @@
 import Head from "next/head";
-
-import { useQuery } from "@tanstack/react-query";
-import axiosInstanceAuthorization from "@/lib/axiosInstanceAuthorization";
-
 export const HeadAdmin = () => {
-  const { data: profileHead, refetch: refetchProfileHead } = useQuery({
-    queryKey: ["profileHead"],
-    queryFn: async () => {
-      const { data } = await axiosInstanceAuthorization.get("/profile");
-      return data[0];
-    },
-  });
-
   return (
     <Head>
-      <title>{profileHead ? profileHead.organization_name : "Admin Organization"}</title>
+      <title>Smoothly - Admin</title>
       <meta
-        name={profileHead ? profileHead.organization_name : "Loading.."}
-        content="admin page for eventease app"
+        name="Smoothly"
+        content="admin page for smoothly app"
       />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/logo.svg" />
