ef42180797b2ac8d2d0ea2422267f3976aab48ab
ghufron akbar <alan.040219.gam@gmail.com>
2024-06-03 01:43:31 +0700

    Edit Lokasi Toko

--- src/components/detail/DetailProfile.jsx
+++ src/components/detail/DetailProfile.jsx
@@ -9,12 +9,34 @@ import {
   Input,
   Button,
   useToast,
+  Stack,
+  Text,
+  Table,
+  Tr,
+  Th,
+  TableContainer,
+  TableCaption,
+  Td,
+  position,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalBody,
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+  CloseButton,
+  ModalCloseButton,
+  ModalFooter,ModalHeader
 } from "@chakra-ui/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import axiosInstanceAuthorization from "@/lib/axiosInstanceAuthorization";
 import { LoadingComponent } from "../LoadingComponent";
 import { Loading } from "../Loading";
+import { primaryColor, white } from "@/lib/color";
+
 
 export function DetailProfile() {
   const toast = useToast();
@@ -22,13 +44,23 @@ export function DetailProfile() {
   const [nama, setNama] = useState("");
   const [logo, setLogo] = useState(null);
   const [username, setUsername] = useState("");
-  const [latitude, setLatitude] = useState("");
-  const [longitude, setLongitude] = useState("");
+  const [tokoLatitude, setLatitude] = useState("");
+  const [tokoLongitude, setLongitude] = useState("");
   const [oldPassword, setOldPassword] = useState("");
   const [newPassword, setNewPassword] = useState("");
   const [confirmationPassword, setConfirmationPassword] = useState("");
   const [profile, setProfile] = useState(null);
   const [error, setError] = useState(false);
+  const [nowLatitude, setNowLatitude] = useState("");
+  const [nowLongitude, setNowLongitude] = useState("");
+  const [isLocationOpen, setIsLocationOpen] = useState(false);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setNowLatitude(position.coords.latitude);
+      setNowLongitude(position.coords.longitude);
+    });
+  });
 
   const { data: dataProfile, refetch: refetchDataProfile } = useQuery({
     queryKey: ["profile"],
@@ -70,13 +102,82 @@ export function DetailProfile() {
     }
   };
 
+  const ModalSetLocation = () => {
+    return (
+      <Modal
+        size="xl"
+        isOpen={isLocationOpen}
+        onClose={() => setIsLocationOpen(false)}
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Ganti Lokasi Toko</ModalHeader>
+          <ModalBody>
+            Apakah anda yakin ingin mengganti lokasi toko dengan lokasi terkini?
+            <Alert my={4} status="info">
+              <AlertIcon />
+              <Box flex="1">
+                <AlertTitle>Pesan!</AlertTitle>
+                <AlertDescription>
+                  Aksi tidak dapat dikembalikan
+                </AlertDescription>
+              </Box>             
+            </Alert>
+          </ModalBody>
+          <ModalCloseButton />
+          <ModalFooter>
+            <Button
+              bg={primaryColor}
+              color={white}
+              onClick={() => {
+                handleLocation()
+              }}
+            >
+              Konfirmasi
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    );
+  };
+
+  const handleLocation = async () => {
+    console.log({nowLatitude,nowLongitude})
+    try {
+      const response = await axiosInstanceAuthorization.put(`/location`, {
+        toko_latitude: nowLatitude,
+        toko_longitude: nowLongitude,
+      });
+
+      toast({
+        title: response.data.message,
+        status: "success",
+        position: "bottom-right",
+        isClosable: true,
+      });
+      refetchDataProfile();
+      setIsLocationOpen(false)
+    } catch (error) {
+      console.log(error);
+      toast({
+        title: error.response.data.message,
+        status: "error",
+        position: "bottom-right",
+        isClosable: true,
+      });
+    }
+  };
+
   const handlePassword = async () => {
     try {
-      const response = await axiosInstanceAuthorization.put(`/profile/password`, {
-        old_password: oldPassword,
-        new_password: newPassword,
-        confirmation_password: confirmationPassword,
-      });
+      const response = await axiosInstanceAuthorization.put(
+        `/profile/password`,
+        {
+          old_password: oldPassword,
+          new_password: newPassword,
+          confirmation_password: confirmationPassword,
+        }
+      );
       toast({
         title: response.data.message,
         status: "success",
@@ -85,7 +186,7 @@ export function DetailProfile() {
       });
       setOldPassword("");
       setNewPassword("");
-      setConfirmationPassword("");      
+      setConfirmationPassword("");
     } catch (error) {
       console.error("Error changing password:", error);
       toast({
@@ -103,7 +204,7 @@ export function DetailProfile() {
   return (
     <>
       {profile && (
-        <Flex justifyContent="center">
+        <Stack>
           <Box
             p={8}
             borderWidth="1px"
@@ -112,62 +213,119 @@ export function DetailProfile() {
             m={4}
             flex={2}
           >
-            <form>
-              <FormControl my={6}>
-                <FormLabel>Nama</FormLabel>
-                <Input value={nama} onChange={(e) => setNama(e.target.value)} />
-              </FormControl>
-              <FormControl my={6}>
-                <FormLabel>Username</FormLabel>
-                <Input
-                  value={username}
-                  onChange={(e) => setUsername(e.target.value)}
-                />
-              </FormControl>
-              <VStack>
-                <Button onClick={handleUpdate}>Update</Button>
-              </VStack>
-            </form>
+            <FormLabel>Lokasi Toko Saat Ini</FormLabel>
+            <Table>
+              <TableContainer>
+                <Tr>
+                  <Th>Latitude</Th>
+                  <Td>{tokoLatitude}</Td>
+                </Tr>
+                <Tr>
+                  <Th>Longitude</Th>
+                  <Td>{tokoLongitude}</Td>
+                </Tr>
+              </TableContainer>
+            </Table>
+            <Center>
+              <Button
+                color={white}
+                bg={primaryColor}
+                mt={4}
+                onClick={() => {
+                  setIsLocationOpen(true);
+                }}
+              >
+                Ganti Dengan Lokasi Terkini
+              </Button>
+            </Center>
           </Box>
-          <Box
-            p={8}
-            borderWidth="1px"
-            borderRadius="lg"
-            overflow="hidden"
-            m={4}
-            flex={1}
-          >
-            <form>
-              <FormControl my={6}>
-                <FormLabel>Old Password</FormLabel>
-                <Input
-                  type="password"
-                  value={oldPassword}
-                  onChange={(e) => setOldPassword(e.target.value)}
-                />
-              </FormControl>
-              <FormControl my={6}>
-                <FormLabel>New Password</FormLabel>
-                <Input
-                  type="password"
-                  value={newPassword}
-                  onChange={(e) => setNewPassword(e.target.value)}
-                />
-              </FormControl>
-              <FormControl my={6}>
-                <FormLabel>Confirmation Password</FormLabel>
-                <Input
-                  type="password"
-                  value={confirmationPassword}
-                  onChange={(e) => setConfirmationPassword(e.target.value)}
-                />
-              </FormControl>
-              <VStack>
-                <Button onClick={handlePassword}>Update</Button>
-              </VStack>
-            </form>
-          </Box>
-        </Flex>
+          <Flex justifyContent="center">
+            <Box
+              p={8}
+              borderWidth="1px"
+              borderRadius="lg"
+              overflow="hidden"
+              m={4}
+              flex={2}
+            >
+              <form>
+                <FormControl my={6}>
+                  <FormLabel>Nama</FormLabel>
+                  <Input
+                    value={nama}
+                    onChange={(e) => setNama(e.target.value)}
+                  />
+                </FormControl>
+                <FormControl my={6}>
+                  <FormLabel>Username</FormLabel>
+                  <Input
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                  />
+                </FormControl>
+                <VStack>
+                  <Button
+                    bg={primaryColor}
+                    color={white}
+                    onClick={() => {
+                      handleUpdate();
+                    }}
+                  >
+                    Edit Profile
+                  </Button>
+                </VStack>
+              </form>
+            </Box>
+            <Box
+              p={8}
+              borderWidth="1px"
+              borderRadius="lg"
+              overflow="hidden"
+              m={4}
+              flex={1}
+            >
+              <form>
+                <FormControl my={6}>
+                  <FormLabel>Old Password</FormLabel>
+                  <Input
+                    type="password"
+                    value={oldPassword}
+                    onChange={(e) => setOldPassword(e.target.value)}
+                  />
+                </FormControl>
+                <FormControl my={6}>
+                  <FormLabel>New Password</FormLabel>
+                  <Input
+                    type="password"
+                    value={newPassword}
+                    onChange={(e) => setNewPassword(e.target.value)}
+                  />
+                </FormControl>
+                <FormControl my={6}>
+                  <FormLabel>Confirmation Password</FormLabel>
+                  <Input
+                    type="password"
+                    value={confirmationPassword}
+                    onChange={(e) => setConfirmationPassword(e.target.value)}
+                  />
+                </FormControl>
+                <VStack>
+                  <Button
+                    color={white}
+                    bg={primaryColor}
+                    onClick={() => {
+                      handlePassword();
+                    }}
+                  >
+                    Edit Password
+                  </Button>
+                </VStack>
+              </form>
+              
+              <ModalSetLocation/>
+            </Box>
+          </Flex>
+        </Stack>
       )}
     </>
   );
